d99e553c3fbda3a41edba013fb3090930490bc89
Pedro Kauã <pk.bleite@gmail.com>
2022-09-15 14:02:10 -0300

    mandandoCodigo

---
+++ calculator.js
@@ -0,0 +1,3 @@
+let jorge = document.querySelectorAll('button');
+//get every item  defined as a class named button, by using it one can make a array out of those nodes.
+console.log(Array.from(jorge));
