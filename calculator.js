befa86bb341c68f4811263d27bfd70f19a4dd000
Pedro Kauã <pk.bleite@gmail.com>
2022-09-17 01:17:21 -0300

    calculatorCanNowDisplay

--- calculator.js
+++ calculator.js
@@ -1,28 +1,27 @@
 const buttons = Array.from(document.getElementsByClassName("button"));
 //get every item  defined as a class named button, by using it one can make a array out of those nodes
-let firstIn = undefined;
-let secondIn = undefined;
-let thirdIn = undefined
 whichDidIPress()
 function whichDidIPress()   {
 buttons.forEach(button => {
-    button.addEventListener("click", () =>{
-        firstIn = button.textContent
+    button.addEventListener("click", () =>
+    {   //get a lot of inputs
+
+        arrayResult = button.textContent
+        return getInputFunc(arrayResult);
     })
 })
-buttons.forEach(button => {
-    button.addEventListener("click", () =>{
-        secondIn = button.textContent
-    })
-})
-buttons.forEach(button => {
-    button.addEventListener("click", () =>{
-        thirdIn = button.textContent
-    })
-})
-return getInputFunc(firstIn, secondIn, thirdIn)
 }
 
-function getInputFunc(inputReceiveF, inputReceiveS, inputReceiveT) {
-    document.querySelector("#getInput").value = inputReceiveF + " " + inputReceiveS + " " + inputReceiveT;
+function getInputFunc(inputReceive) {
+    //concatenates the value + the giving input
+    if(inputReceive == 'DELETE'){
+        inputReceive = "";
+        document.querySelector("#getInput").value = "" ;
+    }
+   let displayContent = document.querySelector("#getInput").value += inputReceive ;
+    return validateDisplay(displayContent);
+}
+
+function validateDisplay(displayContent){
+  
 }
