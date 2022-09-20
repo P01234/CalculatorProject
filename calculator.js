28734b79eb84f042fe03491baf89841a58cd1ce9
Pedro Kauã <pk.bleite@gmail.com>
2022-09-20 16:18:54 -0300

    displaySolved

--- calculator.js
+++ calculator.js
@@ -1,51 +1,58 @@
-const getNumbers = Array.from(document.getElementsByClassName("button"));
-const getOperator = Array.from(document.getElementsByClassName("operator"));
-let previouslyNumber = Array("");
-let actualNumber = Array("");
-let usingOperator = "";
-let nGet = ""
-let usingOperator2 = undefined;
-let getDisplay = document.querySelector("#getInput").value
-
-
-let getNumber = function(){
-    getNumbers.forEach(button =>{
-        button.addEventListener("click", () => {
-            previouslyNumber = button.textContent;
-            nGet = actualNumber += previouslyNumber;
-            //return nGet to another function.
-            return validateEverything(previouslyNumber, actualNumber)
-        })
-    })
+const numbers = Array.from(document.querySelectorAll("[data-number]"));
+const operator = document.querySelectorAll("[data-operator]");
+let operandGetter = "";
+let operatorCheck = false;
+const leftPharentsis = document.querySelector("[data-open]");
+const rightPharentisis = document.querySelector("[data-close]");
+const persentageButton = document.querySelector("[data-percentage]");
+const deleteButton = document.querySelector("[data-delete]");
+let display
+//get operator number and set the boolean which will be used to get the second input.
+let selectOperator = (operatorGetter) =>{
+    let operatorValue = operatorGetter;
+    operatorCheck = false;
+    return validateMath(operatorValue);
 }
-getNumber()
-//to break when operator shows up. 
-/*
-if(previouslyNumber == '+'){
-                usingOperator == previouslyNumber;
-                previouslyNumber == "";
-            }
-            usingOperator2 = parseFloat(usingOperator);
-            console.log(usingOperator2) HappyFace!
-*/
-
-let getOperatorSim = function(){
-    getOperator.forEach(operator =>{
-        operator.addEventListener("click", () =>{
-            usingOperator = operator.textContent; 
-            
-        })
-    })
+//get user's input and send it to display.
+let getOperand = (operand) =>{
+    operandGetter = operand
+    getDisplay(operandGetter)
 }
+//get operand, if operatorCheck exists then it'll get the second input.
+let getDisplay = (displayOperand) =>{
+    if(operatorCheck === false)
+    {
+        display = document.querySelector("#getInput").textContent = displayOperand
+    operatorCheck = true;
+    let firstOperandHolder = display;
+    }else if(displayOperand === "DELETE")
+    {
+        operandGetter = ""
+        operatorCheck = ""
+        display = document.querySelector("#getInput").textContent = "";
 
-function validateEverything( pastN, nowN){
-    console.log(usingOperator + "\n" + pastN + "\n" + nowN)
-    if(usingOperator = "+"){
-        
-        nowN = pastN
-        pastN = ""
-        getDisplay = document.querySelector("#getInput").value = nowN + "+" + pastN;
+    }else
+    {
+        display = document.querySelector("#getInput").textContent += displayOperand
+        let secondOperandHolder = display;
+        console.log(secondOperandHolder)
     }
-     
-    
 }
+
+let validateMath = (mathValidator) =>{
+    switch(mathValidator){
+
+    }
+}
+numbers.forEach(button => {
+    button.addEventListener("click", () => {
+        getOperand(button.textContent)
+        
+    })
+})
+
+operator.forEach(operatorValue => {
+    operatorValue.addEventListener("click", () =>{
+        selectOperator(operatorValue.textContent)
+    })
+})
