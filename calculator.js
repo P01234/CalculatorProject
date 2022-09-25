4b50b7dd3429a4290199fe544d706f08e177f167
Pedro Kauã <pk.bleite@gmail.com>
2022-09-25 16:49:00 -0300

    thisOneIsNotWorking

--- calculator.js
+++ calculator.js
@@ -1,72 +1,81 @@
-const numbers = Array.from(document.querySelectorAll("[data-number]"));
-const operator = document.querySelectorAll("[data-operator]");
-let operandGetter = "";
-let operatorCheck = false;
-let operatorValue1;
-const leftPharentsis = document.querySelector("[data-open]");
-const rightPharentisis = document.querySelector("[data-close]");
-const persentageButton = document.querySelector("[data-percentage]");
-const deleteButton = document.querySelector("[data-delete]");
-let display
-let firstOperandHolder;
-let secondOperandHolder;
-//get operator number and set the boolean which will be used to get the second input.
-let selectOperator = (operatorGetter) =>{
-    operatorValue1 = operatorGetter;
-    operatorCheck = false;
-}
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+let actual;
+document.addEventListener("DOMContentLoaded", function(){
+    let numbers = document.querySelectorAll(".button");
+    let operators = document.querySelectorAll(".operator")
+    let currentNumber = document.querySelector("#getFirstInput");
+    let previousNumber = document.querySelector("#getSecondInput");
+    let equals = document.querySelector("#equals").addEventListener("click", operate);
+    let roundButton = document.querySelector("#roundButton");
+    let deleteNumber = document.querySelector("#delete").addEventListener("click", deleteAll);
 
-//get user's input and send it to display.
-let getOperand = (operand) =>{
-    operandGetter = operand
-    getDisplay(operandGetter)
-}
-//get operand, if operatorCheck exists then it'll get the second input.
-let getDisplay = (displayOperand) =>{
-    if(operatorCheck === false)
-    {
-        display = document.querySelector("#getInput").textContent = displayOperand
-    operatorCheck = true;
-    operandHolder = displayOperand
-    firstOperandHolder = operandHolder
-    }else if(displayOperand === "DELETE")
-    {
-        operandGetter = ""
-        operatorCheck = ""
-        display = document.querySelector("#getInput").textContent = "";
-
-    }else
-    {
-        display = document.querySelector("#getInput").textContent += displayOperand
-        secondOperandHolder = display;
-        console.log(secondOperandHolder)
-        
-    }if(displayOperand === "="){
-        return validateMath(operatorValue1, firstOperandHolder,secondOperandHolder)
+    let defineOpe = function(operatorGet){
+        operator = operatorGet;
     }
-}
 
-let validateMath = (mathValidator, firstOperandHolder,secondOperandHolder) =>{
-    let firstValue = parseFloat(firstOperandHolder);
-    let secondValue = parseFloat(secondOperandHolder);
-    console.log(firstValue)
-    console.log(secondValue)
-    let result;
-    switch(mathValidator){
-        case"+": result = firstValue + secondValue; 
-        case"-": result = firstValue + secondValue;
-   }
-   console.log(result);
-}
-numbers.forEach(button => {
-    button.addEventListener("click", () => {
-        getOperand(button.textContent)
-        
-    })
-})
+    let getNumber = function(number){
+        currentValue = number;
+        if(operator === ""){
+        currentNumber.textContent += currentValue;
+        }else if(currentNumber.textContent.includes(".")){
+            currentNumber.textContent += currentNumber + ","
+        }
+        else{
+            displayUpdate();
+        }
+    }
 
-operator.forEach(operatorValue => {
-    operatorValue.addEventListener("click", () =>{
-        selectOperator(operatorValue.textContent)
+
+
+    let displayUpdate = function(){
+        if(previousValue === ""){
+            previousValue = currentValue;
+            currentValue = "";
+            previousNumber.textContent = currentNumber.textContent+operator;
+        }else if(previousValue !== ""){
+            previousValue = "";
+            previousValue = currentValue;
+            currentValue = "";
+            previousNumber.textContent += `${previousValue}`;
+        }
+    }
+
+    function operate(){
+        let firstValue = parseFloat(currentNumber.textContent);
+        let secondValue = parseFloat(previousNumber.textContent);
+        let result;
+        switch(operator){
+            case"+":
+                result = firstValue + secondValue
+                currentNumber.textContent = result;
+            case"-":
+        }
+    }
+
+    function deleteAll(){
+        operator = "";
+        previousValue = "";
+        currentValue = "";
+        currentNumber.textContent = "";
+        previousNumber.textContent = "";
+
+    }
+
+    operators.forEach(operatorDefiner =>{
+        operatorDefiner.addEventListener("click", () =>{
+            defineOpe(operatorDefiner.textContent);
+        })
     })
-})
+
+    numbers.forEach(defineNumber => {
+        defineNumber.addEventListener("click", () =>{
+            getNumber(defineNumber.textContent);
+        })
+    });
+
+    console.log(operator)
+
+
+});
