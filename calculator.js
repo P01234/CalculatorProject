c182f1138aef6c2894d1f3823fe5ee81bd3a661b
Pedro Kauã <pk.bleite@gmail.com>
2022-09-27 18:01:21 -0300

    logicWorking

--- calculator.js
+++ calculator.js
@@ -1,32 +1,110 @@
-const buttons = Array.from(document.getElementsByClassName("button"));
-let prevValue = "";
-let actualValue = "";
-//get every item  defined as a class named button, by using it one can make a array out of those nodes
-whichDidIPress()
-function whichDidIPress()   {
-buttons.forEach(button => {
-    button.addEventListener("click", () =>
-    {   //get a lot of inputs
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+document.addEventListener("DOMContentLoaded", function(){
+    let numbers = document.querySelectorAll(".button");
+    let operators = document.querySelectorAll(".operator")
+    let currentNumber = document.querySelector("#getFirstInput");
+    let previousNumber = document.querySelector("#getSecondInput");
+    let currentValueHolder;
+    let equals = document.querySelector("#equals").addEventListener("click", operate);
+    let roundButton = document.querySelector("#roundButton");
+    let deleteNumber = document.querySelector("#delete").addEventListener("click", deleteAll);
+    
+    let defineOpe = function(operatorGet){
+        operator = operatorGet;
+    }
 
-        arrayResult = button.textContent
-        return getInputFunc(arrayResult);
+    let getNumber = function(number){
+        currentValue = number;
+        if(operator === ""){
+            currentValueHolder = currentNumber.textContent += currentValue;
+        }else if(currentNumber.textContent.includes(".")){
+            while(currentNumber.text == ","){
+                currentNumber.textContent = currentValue;
+            }
+        }
+        else{
+            display();
+        }
+    }
+
+
+
+    let display = function(){
+        if(previousValue === ""){
+            previousValue = currentValue;
+            currentValue = "";
+            currentNumber.textContent = currentValueHolder+operator;
+            currentValueHolder = "";
+        }else{
+            displayUpdate();
+        }
+        
+    }
+
+    let displayUpdate = function(){
+        previousValue = currentValue;
+        currentValue = "";
+        previousNumber.textContent += `${previousValue}`;
+    }
+
+    function operate(){
+        let firstValue = parseFloat(currentNumber.textContent);
+        let secondValue = parseFloat(previousNumber.textContent);
+        let result;
+        switch(operator){
+            case"+":
+                result = firstValue + secondValue;
+                currentNumber.textContent = result;
+                break;
+            case"-":
+                result = firstValue - secondValue;
+                currentNumber.textContent = result;
+                break;
+            case"/":
+                result = firstValue / secondValue;
+                currentNumber.textContent = result;
+                break;
+            case"x": 
+                result = firstValue * secondValue; 
+                currentNumber.textContent = result;
+                break;
+            case"^":
+                result = firstValue ** secondValue;
+                currentNumber.textContent = result;
+                break;
+            case"%":
+                result = firstValue % secondValue;
+                currentNumber.textContent = result;
+                break;
+            default:
+                result = firstValue + secondValue;
+                currentNumber.textContent = result;
+                break;
+
+        } 
+    }
+
+    function deleteAll(){
+        operator = "";
+        previousValue = "";
+        currentValue = "";
+        currentNumber.textContent = "";
+        previousNumber.textContent = "";
+
+    }
+
+    operators.forEach(operatorDefiner =>{
+        operatorDefiner.addEventListener("click", () =>{
+            defineOpe(operatorDefiner.textContent);
+        })
     })
-})
-}
 
-function getInputFunc(inputReceive) {
-    //concatenates the value + the giving input
-    if(inputReceive == 'DELETE'){
-        inputReceive = "";
-        document.querySelector("#getInput").value = "" ;
-    }
-   let displayContent = document.querySelector("#getInput").value += inputReceive ;
-    return validateDisplay(displayContent);
-}
-function validateDisplay(analiseDisplay){
-    let toArray = Array(analiseDisplay);
-    if(analiseDisplay && toArray == "+"){
-    toArray.pop();
-    }
-    console.log(toArray);
-}
+    numbers.forEach(defineNumber => {
+        defineNumber.addEventListener("click", () =>{
+            getNumber(defineNumber.textContent);
+        })
+    });
+
+});
